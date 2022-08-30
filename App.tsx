449093ed56e268bc276f27e29635b85b9982e693
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-08-30 09:40:15 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+//costom font
+import AppLoadding from "expo-app-loading";
+import { useFonts } from "expo-font";
+
+import Welcome from "./src/screens/Welcome";
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Lato-Bold": require("./src/assets/fonts/Lato-Bold.ttf"),
+    "Lato-Regular": require("./src/assets/fonts/Lato-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoadding />;
+  }
+  return <Welcome />;
+}
