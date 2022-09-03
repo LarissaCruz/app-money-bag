8c612459afdbd0f32c9b9cfe1e045fcef6cc911c
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-09-03 08:33:29 -0300

    add new page Home

--- src/navigatiors/RootStack.tsx
+++ src/navigatiors/RootStack.tsx
@@ -1,12 +1,16 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, Profiler } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Welcome from "../screens/Welcome";
 import Home from "../screens/Home";
+import { colors } from "../components/colors";
+import Greeting from "../components/Header/Greenting";
+import Profile from "../components/Header/Profile";
+import Avi from "../assets/avi/avata.png";
 
 export type RootStackParamList = {
   Welcome: undefined;
-  Home: undefined;
+  Home: any;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -14,11 +18,40 @@ const Stack = createStackNavigator<RootStackParamList>();
 const RootStack: FunctionComponent = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.graylight,
+            borderBottomWidth: 0,
+            shadowColor: "transparent",
+            shadowOpacity: 0,
+            elevation: 0,
+            height: 120,
+          },
+          headerTintColor: colors.secondary,
+          headerRightContainerStyle: {
+            paddingRight: 25,
+          },
+          headerLeftContainerStyle: {
+            paddingLeft: 10,
+          },
+        }}
+        initialRouteName="Home"
+      >
         <Stack.Screen
-          options={{ headerShown: false }}
           name="Home"
           component={Home}
+          options={{
+            headerTitle: (props) => (
+              <Greeting mainText="Hey Coby" subText="Welcome Back" {...props} />
+            ),
+            headerLeft: () => (
+              <Profile
+                img={Avi}
+                imgContainerStyle={{ backgroundColor: colors.tertiary }}
+              />
+            ),
+          }}
         />
         <Stack.Screen name="Welcome" component={Welcome} />
       </Stack.Navigator>
