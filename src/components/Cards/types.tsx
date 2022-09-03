84f09cdd1cc5ac1fc4e3c35e3b9a03f051645a3a
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2022-09-03 08:35:53 -0300

    add new components

---
+++ src/components/Cards/types.tsx
@@ -0,0 +1,9 @@
+import { ImageSourcePropType } from "react-native";
+
+export interface CardProps {
+  id: number;
+  accountNo: string;
+  balance: number;
+  alias?: string;
+  logo: ImageSourcePropType;
+}
